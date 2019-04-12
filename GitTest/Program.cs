2a6ac3a551539def00b7d0dc506a08e303cf3a7d
bosman-dh <49553457+bosman-dh@users.noreply.github.com>
2019-04-12 13:40:53 +0200

    Feature1 added

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -16,6 +16,8 @@ namespace GitTest
             Console.ReadKey();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            
+            //Feature 1
         }
     }
 }
